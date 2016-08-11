b81de2f027296c6164e05034acc2bb1d5c601cd9
Xijiang Yu <xijiang.yu@gmail.com>
2016-08-11 09:45:26 +0200

    pedsort

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # pop-simulation
+===
+## pedsort.cpp
+* Input: id pa ma other-info
+* Output: id pa ma original-id-name other-info
+* Compile: g++ -O2 -o pedsort pedsort.cpp
+* Usage: ./pedsort < unsorted-pedigree > sorted-pedigree
+
+## lnw139.ped
+* This is a sample pedigree adapted from Lynch and Walsh 1996, pp139.
+
---
+++ pedsort.cpp
@@ -0,0 +1,77 @@
+/**
+ * # ID start from 1, hence ID will be added ne-1 in extended.ped
+ *
+ * This is a new version of pedigree sort, which I have written several times.
+ * Every time I am facing the same question, I feel I can program better. 
+ * This may be a bad habit.
+ *                                                            by Xijiang Yu
+ *                                                            Apr. 13, 2015
+ */
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+class Record{
+public:
+  string id;
+  string pa;
+  string ma;
+  string info;
+  Record(string a, string b, string c, string d):id{a}, pa{b}, ma{c}, info{d}{;}
+};
+
+int main(int argc, char *argv[])
+{
+  const set<string> missing{".", "-", "unknown", "Unknown", "UNKNOWN", "999999",
+      "missing", "Missing", "MISSING"}; //and whatever else can be added here.
+  //All of above will be renamed to "0".
+  const string unknown{"0"};
+  vector<Record> ped;
+  
+  for(string line, id, pa, ma, info; getline(cin, line);){
+    stringstream ss(line);
+    if(ss>>id>>pa>>ma){ //ignore those that have less than 3 fields.
+      getline(ss,info);
+      if(missing.find(pa) != missing.end()) pa=unknown;
+      if(missing.find(ma) != missing.end()) ma=unknown;
+      ped.push_back(Record(id,pa,ma,info));
+    }else clog<<"Ignored: "<<line<<'\n';
+  }
+
+  map<string, int> uid;
+  for(auto&p:ped){//Suppose unknown ID all appear in pa & ma columns
+    uid[p.pa] = -2;
+    uid[p.ma] = -2;
+  }
+
+  // ID in the id column
+  for(auto&p:ped) uid[p.id] = -1;
+  uid[unknown] = 0;
+
+  // ID only appear in parent columns
+  int id{0}, td(uid.size()-1);
+  for(auto&p:uid) if(p.second == -2){
+      p.second=++id;
+      cout<<id<<" 0 0 Unknown\n";
+    }
+  
+  // Then all the ID in the ID column.
+  while(td)
+    for(auto&p:ped)
+      if(uid[p.id] ==-1 &&
+	 uid[p.ma] >= 0 &&
+	 uid[p.ma] >= 0){
+	++id;
+	cout<<id<<' '<<uid[p.pa]<<' '<<uid[p.ma]<<' '<<p.id<<' '<<p.info<<'\n';
+	uid[p.id] = id;
+	--td;
+      }
+
+  return 0;
+}
